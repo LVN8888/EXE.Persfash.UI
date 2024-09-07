9aa3c5c96b4ad60c58d06d656a0cd4e6ba58e0b2
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-09-07 23:00:47 +0700

    [Update] Update code base & footer.

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us 🤔</h1>
+      <p>We are just having some fun with React components!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../components/layouts/Footer';
+
+
+function Home() {
+  return (
+    <div>
+      <h1 >The huhuhu! 🏠</h1>
+      <p>This is where your React journey begins. Enjoy coding!</p>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
