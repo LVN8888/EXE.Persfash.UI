3edad84a85c9a8517fd3d335207a668db6b22929
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-09-08 19:31:01 +0700

    [Update] Update css of homepage

--- src/components/partials/Home/index.jsx
+++ src/components/partials/Home/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './style.module.scss';
-import secondImage from '../../../assets/img/bg_2.png'; // Import the second background image
+import secondImage from '../../../assets/img/bg_2.png'; // Second section background image
+import pic3 from '../../../assets/img/pic_3.png'; // Third image
+import pic4 from '../../../assets/img/pic_4.png'; // Fourth image
 
 const Home = () => {
   return (
@@ -17,12 +19,32 @@ const Home = () => {
       {/* Gap between sections */}
       <div className={styles.space}></div>
 
-      {/* Second Section with Background Image */}
+      {/* Second Section */}
       <div className={styles.secondContent}>
         <div className={styles.secondContentText}>
           <h2>What can we do?</h2>
           <p>Just know that I would die for you</p>
           <p>Baby, I would die for you, yeah</p>
+        </div>
+      </div>
+
+      {/* Third Section with two cards */}
+      <div className={styles.thirdContent}>
+        <div className={styles.card} style={{ backgroundImage: `url(${pic3})` }}>
+          <div className={styles.cardTitle}>
+            <h2>A.I</h2>
+          </div>
+          <div className={styles.cardText}>
+            <p>Simultaneously, your long-term vision encompasses the establishment of your own studio. Armed with the knowledge and experience gained from your early career, you envision a space where you can cater to both individual clients and larger-scale projects.</p>
+          </div>
+        </div>
+        <div className={styles.card} style={{ backgroundImage: `url(${pic4})` }}>
+          <div className={styles.cardTitle}>
+            <h2>Consulting with experts</h2>
+          </div>
+          <div className={styles.cardText}>
+            <p>Simultaneously, your long-term vision encompasses the establishment of your own studio. Armed with the knowledge and experience gained from your early career, you envision a space where you can cater to both individual clients and larger-scale projects.</p>
+          </div>
         </div>
       </div>
     </div>
