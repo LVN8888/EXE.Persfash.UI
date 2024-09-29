d727e9e454ac6334fc5d5f970a780e287dd17f49
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-09-29 15:05:34 +0700

    [Feature] Complete fetch API from backend of UserManagement Page

--- src/services/PayOSApi.jsx
+++ src/services/PayOSApi.jsx
@@ -1,14 +1,14 @@
 import AxiosHelper from "../AxiosHelper";
 
 const BaseURL = import.meta.env.VITE_SERVER_URL;
-const apiClient = new AxiosHelper(BaseURL)
+const apiClient = new AxiosHelper(BaseURL);
 
 export const viewPaymentResult = async (orderCode) => {
-    try {
-        const response = await apiClient.get("/payos", {orderCode})
+  try {
+    const response = await apiClient.get("/payos", { orderCode });
 
-        return response;
-    }catch(error) {
-        throw error
-    }
-} 
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
---
+++ src/services/UserManagementApi.jsx
@@ -0,0 +1,26 @@
+import AxiosHelper from "../AxiosHelper";
+
+const BaseURL = import.meta.env.VITE_SERVER_URL;
+const apiClient = new AxiosHelper(BaseURL);
+
+// API to get a list of customers with pagination (including token)
+export const viewCustomerList = async (page, size) => {
+    try {
+        // `true` để chỉ định rằng API yêu cầu kiểm tra token
+        const response = await apiClient.get("/customer/view", { page, size }, {}, true);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// API to activate or deactivate a customer by customerId (including token)
+export const activateDeactivateCustomer = async (customerId) => {
+    try {
+        // `true` để chỉ định rằng API yêu cầu kiểm tra token
+        const response = await apiClient.put(`/customer/activate-deactivate/${customerId}`, {}, {}, true);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
